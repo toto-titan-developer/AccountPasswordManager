da6733b6b599995c0ab0fcf0f62641815d3f94bb
whenderson <henderson.wyatt95@gmail.com>
2026-05-24 17:36:40 -0400

    Added summary and initial information to the Program cs file

--- AccountPasswordManager/Program.cs
+++ AccountPasswordManager/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Wyatt!");
+﻿////
+///Summary
+///Project Account Password Manager - Project 1 - INFO3138
+///Conttributers: Wyatt Henderson, Joe Whitton
+///Description: Using JSON serialization and parsing to pull and validate a JSOn file of account information with a JSON Schema
+///Start Date: May 24, 2026
+///Due Date: June 5th 2026
+///
+
+namespace AccountPasswordManager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello party people!");
+
+
+
+        }
+    }
+}
